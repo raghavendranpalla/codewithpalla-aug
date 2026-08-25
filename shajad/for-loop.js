50d387febb475602156fbf0152be8d67f8b0c44f
shajad-shaik <shajad.shaik@gmail.com>
2026-08-25 10:33:45 +0530

    wrote code using for loop

---
+++ shajad/for-loop.js
@@ -0,0 +1,24 @@
+
+/* Print 1 - 10 numbers*/
+
+// for (let i = 1; i <= 10; i++)
+// {
+//     console.log (i);
+// }
+
+/* Print even numbers between 1 - 20*/
+
+for (let i = 1; i<=20; i++)
+{
+    if (i%2 === 0){
+        console.log (i);
+    }
+}
+
+/* Print odd numbers between 1 - 100 */
+
+// for (let i = 1; i<=100; i++){
+//     if (i%2 !== 0){
+//         console.log (i);
+//     }
+// }
